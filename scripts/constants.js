61add28837a3c82791f87d5925274a2295a3846f
adeafblindman <adeafblindman@gmail.com>
2024-02-18 17:07:57 -0600

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,7 +2,7 @@ var constants = {
   // Tabs in Google Sheet
   notesSheetName: 'Notes',
 	optionsSheetName: 'Options',
-	pointsSheetName: 'SubmitPoints',
+	pointsSheetName: 'Points',
   polygonsSheetName: 'Polygons',
   polylinesSheetName: 'Polylines',
   /* OPTIONS */
