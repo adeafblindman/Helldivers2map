f66fdc4dd0e62ce94a2176ad68be1a67498595ed
adeafblindman <adeafblindman@gmail.com>
2024-02-26 22:34:39 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qqjNpXTZqEeUDtcYyRzzfbxnUZyRPErEVKAJys1y2zw/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/17AtGATs6x_IJeBUoHklup6fli1E3aYUMaPQsOuP8NFc';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAicGfw_wfI43khG8Z11i4Xa4CGsKcI3WE';
